5edbc6c4299a12bc1822a7eabfc8420020b69ae4
Don Sartain <don.sartain@benefitmall.com>
2020-09-24 14:31:52 -0500

    Added Authorize attribute

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
+#if !Debug
+    [Authorize]
+#endif
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
